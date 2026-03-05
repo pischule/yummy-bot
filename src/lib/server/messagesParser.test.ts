5b1401050b736a2f269dad28c647372900dc32cc
Maksim Pischulenok <pischule@outlook.com>
2026-03-05 23:36:51 +0300

    Refactor parser

--- src/lib/server/messagesParser.test.ts
+++ src/lib/server/messagesParser.test.ts
@@ -2,63 +2,92 @@ import { describe, it, expect } from 'vitest';
 import { ordersToTsv } from './messagesParser';
 
 describe('ordersToTsv', () => {
-  const mockRawText = `
-YummyOrderBot, [01.01.24 12:00]
-Alice:
-- Burger x2
-- Fries
-
-YummyOrderBot, [01.01.24 12:05]
-Bob:
-- Soda x3
-- Burger
-`;
-
-  const mockRawNewText = `
-[01.01.24 12:00] YummyOrderBot: Alice:
-- Burger x2
-- Fries
-[01.01.24 12:00] SomeSender: QWE:
-- Similar looking format
-someJunkText
-[01.01.24 12:05] YummyOrderBot: Bob:
-- Soda x3
-- Burger
-`;
-
-  const mockAnotherText = `
-YummyOrderBot, [3 Mar 2026 at 12:58:10]:
-Alice:
-- Burger x2
-- Fries
-
-
-YummyOrderBot, [3 Mar 2026 at 13:09:58]:
-Bob:
-- Soda x3
-- Burger
-`;
-
   const mockMenu = ['Soda', 'Burger', 'Fries'];
 
-  it('should correctly parse names and quantities', () => {
-    const result = ordersToTsv(mockRawText, mockMenu);
+  it('should parse old win format', () => {
+    const text = `
+    YummyOrderBot, [01.01.24 12:00]
+    Alice:
+    - Burger x2
+    - Fries
+
+    YummyOrderBot, [01.01.24 12:05]
+    Bob:
+    - Soda x3
+    - Burger
+    `;
+
+    const result = ordersToTsv(text, mockMenu);
+    const lines = result.split('\n');
+
+    expect(lines).toStrictEqual([
+      'Имя\t"Soda"\t"Burger"\t"Fries"',
+      '"Alice"\t\t2\t1',
+      '"Bob"\t3\t1\t',
+    ]);
+  });
+
+  it('should parse old win format with extra colon', () => {
+    const text = `
+    YummyOrderBot, [01.01.24 12:00]:
+    Alice:
+    - Burger x2
+    - Fries
+
+    YummyOrderBot, [01.01.24 12:05]:
+    Bob:
+    - Soda x3
+    - Burger
+    `;
+
+    const result = ordersToTsv(text, mockMenu);
+    const lines = result.split('\n');
+
+    expect(lines).toStrictEqual([
+      'Имя\t"Soda"\t"Burger"\t"Fries"',
+      '"Alice"\t\t2\t1',
+      '"Bob"\t3\t1\t',
+    ]);
+  });
+
+  it('should parse mac format', () => {
+    const text = `
+    > YummyOrderBot:
+    Alice:
+    - Burger x2
+    - Fries
+
+    > OtherSender:
+    Todd:
+    - qwe
+
+    > YummyOrderBot:
+    Bob:
+    - Soda x3
+    - Burger
+    `;
+
+    const result = ordersToTsv(text, mockMenu);
     const lines = result.split('\n');
 
     expect(lines[1]).toBe('"Alice"\t\t2\t1');
     expect(lines[2]).toBe('"Bob"\t3\t1\t');
   });
 
-  it('should correctly parse new format', () => {
-    const result = ordersToTsv(mockRawNewText, mockMenu);
-    const lines = result.split('\n');
+  it('should parse new win format', () => {
+    const text = `
+  [01.01.24 12:00] YummyOrderBot: Alice:
+  - Burger x2
+  - Fries
+  [01.01.24 12:00] SomeSender: QWE:
+  - Similar looking format
+  someJunkText
+  [01.01.24 12:05] YummyOrderBot: Bob:
+  - Soda x3
+  - Burger
+  `;
 
-    expect(lines[1]).toBe('"Alice"\t\t2\t1');
-    expect(lines[2]).toBe('"Bob"\t3\t1\t');
-  });
-
-  it('should correctly parse another format', () => {
-    const result = ordersToTsv(mockAnotherText, mockMenu);
+    const result = ordersToTsv(text, mockMenu);
     const lines = result.split('\n');
 
     expect(lines[1]).toBe('"Alice"\t\t2\t1');
@@ -66,7 +95,19 @@ Bob:
   });
 
   it('should sort columns based on the provided menu order', () => {
-    const result = ordersToTsv(mockRawText, mockMenu);
+    const text = `
+    YummyOrderBot, [01.01.24 12:00]
+    Alice:
+    - Burger
+    - Fries
+
+    YummyOrderBot, [01.01.24 12:05]
+    Bob:
+    - Soda
+    - Burger
+    `;
+
+    const result = ordersToTsv(text, mockMenu);
     const headers = result.split('\n')[0];
 
     // Header should be: Имя, "Soda", "Burger", "Fries"
