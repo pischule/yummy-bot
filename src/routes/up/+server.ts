59fb73b802f80a898aa3f995529618cf979505a6
Maksim Pischulenok <pischule@outlook.com>
2026-03-07 13:39:48 +0300

    Init empty project

--- src/routes/up/+server.ts
+++
@@ -1,6 +0,0 @@
-import type { RequestHandler } from './$types';
-import { json } from '@sveltejs/kit';
-
-export const GET = (async () => {
-  return json({ ok: true });
-}) satisfies RequestHandler;
